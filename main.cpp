f9487c6dda5b8540d604fb055261baa6bdf0dd6e
Csrand <chrystiansenna@gmail.com>
2024-09-18 19:37:06 -0300

    exercicio2

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	{
+		int b;
+		int a;
+	using namespace std;
+	cout <<"**************************************"<<"\n";
+	cout <<"******** CALCULADORA DE AREA *********"<<"\n";
+	cout <<"**************************************"<<"\n";
+	cout <<"informe o valor da base"<<"\n";
+	cin >>b;
+	cout <<"informe o valor da altura"<<"\n";
+	cin >>a;
+	cout <<"O resultado e: "<<"\n"<< a * b;
+	}
+	
+	return 0;
+}
