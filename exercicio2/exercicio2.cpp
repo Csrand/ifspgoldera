a70acc5f83545885e932c489a709a94e02648583
Csrand <chrystiansenna@gmail.com>
2024-09-20 04:59:30 -0300

    up

---
+++ exercicio2/exercicio2.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int base, altura, area;
+    cout << "*****************************" << "\n";
+    cout << "**** CALCULADORA DE AREA ****" << "\n";
+    cout << "*****************************" << "\n";
+    cout << "Digite o valor da base:" << "\n";
+    cin >> base;
+    cout << "Digite o valor da altura:" << "\n";
+    cin >> altura;
+    area = base * altura;
+    cout <<"Resultado:" << area <<"\n";
+    return 0;
+
+    
+
+
+
+}
