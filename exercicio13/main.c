0f3de10e23c0ccf95dfb630028ddd7f710e9aa9f
Csrand <113949760+Csrand@users.noreply.github.com>
2024-09-21 11:16:48 -0300

    Update main.c

--- exercicio13/main.c
+++ exercicio13/main.c
@@ -5,7 +5,7 @@ int main()
     int num; 
     printf("Calculador de Tabuada:\n");
     printf("Informe um numero:\n");
-        scanf("%d", & num);
+    scanf("%d", & num);
             for (int i = 1; i <= 10; i++)
             {
                 int resultado = num *i;
