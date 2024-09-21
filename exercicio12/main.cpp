05620cf811ca1578d413c58c36dccf121bf0221d
Csrand <113949760+Csrand@users.noreply.github.com>
2024-09-21 11:16:24 -0300

    Update main.cpp

--- exercicio12/main.cpp
+++ exercicio12/main.cpp
@@ -7,11 +7,11 @@ int main()
     float custo_teatro, valor_do_ingresso, vendas_min;
     cout << "Calcule quantos ingressos voce tem que vender "<<"\n";
     cout << "Informe o Custo do Local do Evento:" <<"\n";
-        cin >> custo_teatro;
+    cin >> custo_teatro;
     cout <<"Informe o valor do ingresso:" <<"\n";
-        cin >> valor_do_ingresso;
+    cin >> valor_do_ingresso;
      vendas_min = custo_teatro / valor_do_ingresso;
-     cout << fixed <<setprecision(2);
+    cout << fixed <<setprecision(2);
     cout << "E necessario vender no minimo:" <<"\n" << vendas_min << " ingressos";
     return 0;
     }
