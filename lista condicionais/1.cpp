ebda75da1cde70ea0bb56b469e687a4c12e20f55
Csrand <chrystiansenna@gmail.com>
2024-10-10 01:45:16 -0300

    up

---
+++ lista condicionais/1.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+main(){
+    int n1, n2, n3, maior, menor, meio;
+    cout << "Digite um numero: ";
+    cin >> n1;
+    cout << "Digite outro numero: ";
+    cin >> n2;
+    cout << "Digite outro numero: ";
+    cin >> n3;
+    if (n1 >= n2){
+        if(n1 >= n3){
+            maior = n1;
+            if(n2>=n3){
+                meio = n2;
+                menor = n3;
+
+            }
+        }
+        else{
+            meio = n1;
+            menor = n2;
+            maior = n3;
+        }
+    }
+    else{
+        if(n2 >=n3){
+            maior = n2;
+            if(n1>=n3){
+                meio = n1;
+                menor = n3;
+            }
+            else
+                meio = n3;
+                menor = n1;
+
+        }
+        else{
+            menor = n1;
+            meio = n2;
+            maior = n3;
+
+        }
+    }
+    cout << "Seus numeros em ordem decrescente:\n";
+    cout << maior << " " << meio << "" << menor;
+    return 0;
+}
